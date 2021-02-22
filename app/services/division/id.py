b858b372f81ba2ad870baff76d423b88d2660e23
harsh47 <harsh@namasys.co>
2021-02-22 23:00:49 +0530

    trial 3

--- app/services/division/id.py
+++
@@ -1,9 +0,0 @@
-class DivisionId:
-    #buttons in divison
-    PL = r"""/html/body/div[1]/div[2]/div[3]/div/div/div/div[2]/header/div/div[2]/div/button[2]"""
-    Notes = r"""/html/body/div[1]/div[2]/div[3]/div/div/div/div[2]/header/div/div[2]/div/button[3]"""
-    freq_filter = r"""//*[@id="root"]/div[2]/div[3]/div/div/div/div[1]/div[2]/div/div/div"""
-    year_filter = r"""/html/body/div[1]/div[2]/div[3]/div/div/div/div[1]/div[1]/div/div/div"""
-    business_filter =  r"""//*[@id="root"]/div[2]/div[3]/div/div/div/div[1]/div[5]/div/div/div"""
-    groupby = r"""/html/body/div[1]/div[2]/div[3]/div/div/div/div[1]/div[7]/div/div/div"""
-    apply = r"""//*[@id="root"]/div[2]/div[3]/div/div/div/div[1]/button"""
