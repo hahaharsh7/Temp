6b4f2f14f439ea92ac1c32bc95ead5bdab97b9ad
harsh47 <harsh@namasys.co>
2021-02-22 16:13:08 +0530

    trial 2

---
+++ app/services/homepagetester/id.py
@@ -0,0 +1,5 @@
+class HomeId:
+    """Order is fixed for each class variable. It is define in below format
+        ({Html Element Identifier},{Method to access html identifier})
+    """
+    report = ("""//*[@id="root"]/div[2]/div[3]/div/div/div/div[2]/div[2]/div[1]/div/p""",)
